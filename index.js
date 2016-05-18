eeea02fcffd05fe5631fcbd69fa5e7db55d1d6b0
Mike Perri <me@michaeljperri.com>
2016-05-18 07:40:43 -0500

    Change hapi port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const Hapi = require('hapi');
 const path = require('path');
 const server = new Hapi.Server();
-server.connection({ port: 80 });
+server.connection({ port: 3000 });
 
 server.start((err) => {
     if (err) {
